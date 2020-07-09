c783764037e0336403c048b05f68b39a485af7e5
Moath <moath.dlaii@gmail.com>
2020-07-08 17:43:37 -0700

    created post route

--- public/app.js
+++ public/app.js
@@ -13,13 +13,76 @@ class App extends React.Component{
         )
     }
 
+    createGift = (event) => {
+      event.preventDefault();
+      axios.post(
+          '/gifts',
+          {
+            wisher:this.state.newWisher,
+            item:this.state.newItem,
+            image:this.state.newImage,
+            des:this.state.newDes,
+            link:this.state.newLink,
+
+          }
+      ).then(
+        (response) => {
+          this.setState({
+            gifts:response.data
+          })
+        }
+      )
+
+    }
+
+    createWisher = (event) => {
+      this.setState({
+          newWisher:event.target.value
+      })
+    }
+
+    createItem = (event) => {
+      this.setState({
+          newItem:event.target.value
+      })
+    }
+
+    createImgURL = (event) => {
+      this.setState({
+          newImage:event.target.value
+      })
+    }
+
+    createDes = (event) => {
+      this.setState({
+          newDes:event.target.value
+      })
+    }
+
+    createLink = (event) => {
+      this.setState({
+          newLink:event.target.value
+      })
+    }
+
 
   render = () => {
-    return <div> HELLO REACT
+    return <div> <h1>REACT APP</h1>
+    <div>
+      <form onSubmit={this.createGift}>
+
+       <input onKeyUp={this.createWisher} type="text" placeholder="name" required/><br/>
+       <input onKeyUp={this.createItem} type="text" placeholder="Gift" required/><br/>
+       <input onKeyUp={this.createImgURL} type="text" placeholder="ImageURL" required/><br/>
+       <input onKeyUp={this.createDes} type="text" placeholder="Description" required/><br/>
+       <input onKeyUp={this.createLink} type="text" placeholder="Link" required/><br/>
+       <input type="submit" value="Make a Wish"/>
+      </form>
+    </div>
     {
       this.state.gifts.map(
         (gift) => {
-          return <h1>{gift.wisher}</h1>
+          return <h1>{gift.wisher} : {gift.item}</h1>
         }
       )
     }
